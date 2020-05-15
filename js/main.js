f642623e35707cbb59c8d98f69c8f9ed87c5b436
csslick <digitone@naver.com>
2020-05-15 14:42:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,5 +10,15 @@ $(function(){
     $('#main_gnb').slideToggle();
   });
 
+  // pc, 모바일 GNB 반응형 표시 처리
+  window.addEventListener('resize', function(){
+    var w = window.innerWidth;
+    if(w >= 768) {
+      $('#main_gnb').css('display', 'block');
+    } else {
+      $('#main_gnb').css('display', 'none');
+    }
+  })
+
 
 });
