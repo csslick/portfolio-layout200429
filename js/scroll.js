799848b16b205026892c2c567774e1486e522596
csslick <digitone@naver.com>
2020-05-18 10:26:18 +0900

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  // 내부 링크(#)시 부드러운 스크롤 이동
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
